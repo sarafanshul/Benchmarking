58df67ff8a57bd40d572f068affa48fa888dad2a
sarafanshul <anshulsaraf3@gmail.com>
2020-03-10 15:20:18 +0530

    it KINDA works

--- using_prime_sieve/sieveEuthcpp.cpp
+++ using_prime_sieve/sieveEuthcpp.cpp
@@ -1,41 +1,35 @@
-#include<iostream>
 #include <Python.h>
-#include <vector>
-using namespace std;
 
-
-// void sievecreate(long n , bool prime[]){
-// void sievecreate(long n , vector<bool> &prime){
-void sievecreate(long n , PyObject* prime){
-	const Py_ssize_t N = n;
+void sievecreate(long n , PyObject* prime ,PyObject* false1 , PyObject* true1){
+	Py_ssize_t N = n;
 	for (Py_ssize_t p=2; p*p<=N; p++){ 
-		// if (prime[p] == true) 
-		if ((PyList_GetItem(prime, p) )== (Py_True)){
-			for (Py_ssize_t i=p*p; i<=N; i += p) 
-				// prime[i] = false;
-				PyList_SetItem(prime, i, Py_False); 
+		// if (prime[p] == true1)
+		PyObject *item_idx = PyList_GetItem(prime, p);
+		if (item_idx == true1){
+			for (Py_ssize_t i = p*p; i<=N; i += p){
+				// prime[i] = false1;
+				Py_INCREF(item_idx);
+				PyList_SetItem(prime, i, false1); //
+				Py_DECREF(item_idx);
+			}
 		} 
 	} 
 }
 
 static PyObject *sieve(PyObject *self, PyObject *args) {
 	int input;
+	const Py_ssize_t length = input + 2;
 	if (!PyArg_ParseTuple(args, "i", &input)) {
 		return NULL;
 	}
-
-	// return PyLong_FromLong((long)input * (long)input);
-	// vector<bool> primes ((int)input,true);
-	// bool* primes = new bool[input+1];
-	
-	const Py_ssize_t length = input + 2;
+	PyObject *true1 = Py_True;
+	Py_INCREF(true1);
+	PyObject *false1 = Py_False;
+	Py_INCREF(false1);
 	PyObject* primes = PyList_New(length);
-	PyList_SET_ITEM(primes , input , Py_True);
+	PyList_SET_ITEM(primes , length , true1);//
 
-	sievecreate((long)input ,primes);
-	// return Py_BuildValue("[items]" ,primes);
-	// return Py_BuildValue("[ii]" ,primes); // here
-	// PyObject * python_lst = Py_BuildValue("[ii]", primes);
+	sievecreate((long)input ,primes , false1 ,true1);
     return primes;
 };
 
